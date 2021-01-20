50d6e3b78ccb6325c45da8b7f9629f39cf95eba4
Robert T McLaughlin <robert.mclaughlin@abbvie.com>
2021-01-19 18:34:54 -0800

    tcga test module nearly complete

--- test/tcga_pancancer/1_format_mafs.py
+++ test/tcga_pancancer/1_format_mafs.py
@@ -22,38 +22,40 @@ for patient in patients:
     kit, indication, patient = patient.split('/')[-3:]
     data.append([patient,kit,indication])
     
-df = pd.DataFrame(data, columns=['patient', 'kit', 'indication'])
-df['split'] = df['kit'].apply(lambda x: 'train' if x == 'agilent_custom_v2_kit' else ('validation' if x == 'hgsc_vcrome_kit' else 'test'))
-df['cnv'] = '/bioinformatics/Users/masicdx/tcga/cnv/' + df['patient'] + '.cns'
-df['input_maf'] = '/bioinformatics/Users/masicdx/tcga/maf/' + df['patient'] + '.maf'
-df['input_maf'] = '/fioSeq/projects/output/dna/tcga/' + df['patient'] + '/mutation/' + df['patient'] + '/' + df['patient'] + '.maf'
+data_splits = pd.DataFrame(data, columns=['patient', 'kit', 'indication'])
+data_splits['split'] = data_splits['kit'].apply(lambda x: 'train' if x == 'agilent_custom_v2_kit' else \
+              ('validation' if x == 'hgsc_vcrome_kit' else 'test'))
+data_splits['cnv'] = '/fioSeq/projects/output/dna/tcga/' + data_splits['patient'] + '/cnv/' + \
+         data_splits['patient'] + '/' + data_splits['patient'] + '_tumor.cns'
+data_splits['input_maf'] = '/fioSeq/projects/output/dna/tcga/' + data_splits['patient'] + '/mutation/' + \
+                  data_splits['patient'] + '/' + data_splits['patient'] + '.maf'
 
 
-#df.to_csv('data_splits.csv', index_label=False)
+
+data_splits.to_csv('data_splits_.csv', index_label=False)
 
 
-#maf = pd.read_csv(df.loc[0,'input_maf'], sep='\t')
+#maf = pd.read_csv(data_splits.loc[0,'input_maf'], sep='\t')
 
 if not os.path.exists('mafs/formatted'):
-    os.mkdir('mafs/formatted')
+    os.makedirs('mafs/formatted')
 
 # Drop the old prob_somatic and xgboost_class labels
-for idx in df.index:
-    maf = pd.read_csv(df.loc[idx,'input_maf'], sep='\t')
-    maf.drop(columns=['info_snp', 'fioseq_prob_somatic','xgboost_class'], inplace=True)
-    maf.loc[maf['filter'].isin(['alt_allele_in_normal;germline_risk', 'germline_risk;alt_allele_in_normal', 'germline_risk', 'alt_allele_in_normal']), 'filter'] = maf['filter'] + ';PASS;tumor_only'
-    maf.to_csv(df.loc[idx, 'patient']+'.maf', index_label=False, sep='\t')
+for idx in data_splits.index:
+    single_patient_output_maf_filename = 'mafs/formatted/' + data_splits.loc[idx, 'patient']+'.maf' 
+    if not os.path.exists(single_patient_output_maf_filename):
+        maf = pd.read_csv(data_splits.loc[idx,'input_maf'], sep='\t')
+        maf.drop(columns=['info_snp', 'fioseq_prob_somatic','xgboost_class'], inplace=True)
+        maf.loc[maf['filter'].isin(['alt_allele_in_normal;germline_risk', 'germline_risk;alt_allele_in_normal', 'germline_risk', 'alt_allele_in_normal']), 'filter'] = maf['filter'] + ';PASS;tumor_only'
+        maf.to_csv(single_patient_output_maf_filename, index_label=False, sep='\t')
+    else:
+        print(single_patient_output_maf_filename + ' already exists. Skipping.')
 
-
-# Check one of them. Then I'll move the original mafs to the directory 'sentieon' 
-pd.read_csv('TCGA-ZM-AA0N.maf', sep='\t')
 
 
 # Finally, save data_splits to the directory 'formatted'. Downstream, these are loaded into prob_somatic.
-df['snv'] = '/bioinformatics/Users/masicdx/tcga/maf/formatted/' + df['patient'] + '.maf'
-df.to_csv('data_splits.csv', index_label=False)
-
-
-# Check it!
-#pd.read_csv('data_splits.csv')
+data_splits['snv'] = os.path.dirname(os.path.abspath(__file__)) + '/mafs/formatted/' + data_splits['patient'] + '.maf'
+data_splits.to_csv('data_splits.csv', index_label=False)
+# delete the intermediate file.
+os.remove('data_splits_.csv')
 
---
+++ test/tcga_pancancer/2_prob_somatic_mafs.py
@@ -0,0 +1,14 @@
+"""
+This file imports the function that uses multiprocessing to annotate formatted MAFs with ProbSomatic metrics.
+data_splits.csv is used as input for the cohort.
+"""
+
+import context
+
+import os
+from new_normal.prob_somatic_helpers import get_prob_somatic_mafs
+
+tcga_project_dir = os.path.dirname(os.path.abspath(__file__))
+
+# calculates prob somatic features, annotates mafs with features, and saves mafs 
+get_prob_somatic_mafs(tcga_project_dir)
