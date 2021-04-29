9c011adefe3d5c53042c0d17e4f507da0f0197b6
Robert T McLaughlin <robert.mclaughlin@abbvie.com>
2021-04-29 13:41:03 -0700

    refactor code for compatibility with new ps

--- new_normal/prob_somatic_helpers.py
+++ new_normal/prob_somatic_helpers.py
@@ -18,43 +18,41 @@ PROB_SOMATIC_HYPERPARAMS = {'info_snp_pop' : 0.001, 'info_snp_freq' : 0.95,
                                 'info_snp_depth' : 10.0, 'min_info_snp' : 20.0,
                                 'expand_copy_window_by' : 0.01, 'max_copy_window_size' : 1.0, 'vaf_bin_size' : 0.01}
 
-def process_sample(prob_somatic, sample):
-    '''
-    Called by multiprocess_samples to do one sample/core. 
-    '''
-    prob_somatic.add_samples([sample])
+def calc_prob_somatic(row, hyperparams=None):
+    
+    snv_path =  row.snv
+    cnv_path = row.cnv
+    prob_somatic = ProbSomatic(merge_somatic((snv_path, cnv_path)), **hyperparams)
+    prob_somatic._format_data()
+    prob_somatic._calc_prob()
     
     return prob_somatic.prob_somatic
 
-def multiprocess_samples(samples, params = None, max_cores = None):
+
+def multiprocess_samples(data_splits, params = None, max_cores = None):
     '''
     Ships jobs to process_samples, one sample/core
     '''
     
-    prob_somatic = ProbSomatic(samples,
-                               info_snp_pop = params['info_snp_pop'], 
-                               info_snp_freq = params['info_snp_freq'], 
-                               info_snp_depth = params['info_snp_depth'], 
-                               min_info_snp = params['min_info_snp'], 
-                               expand_copy_window_by = params['expand_copy_window_by'], 
-                               max_copy_window_size = params['max_copy_window_size'],
-                               vaf_bin_size = params['vaf_bin_size'])
         
     dfs = []
     print(f'Running ProbSomatic using {max_cores} cores to process samples in parallel.')
     with Pool(max_cores) as p:
-        dfs.extend(p.map(partial(process_sample, prob_somatic), samples.index.tolist()))
+        dfs.extend(p.map(partial(calc_pro_somatic, hyperparams = hyperparams), data_splits.iterrows()))
 
     return pd.concat(dfs)
 
-def get_prob_somatic_mafs(project_dir, max_cores = None, output_dir = None): 
-    data = pd.read_csv(os.path.join(project_dir,'data_splits.csv'))
+def get_prob_somatic_mafs(project_dir, max_cores = None, output_dir = None, hyperparams = None): 
+    data_splits = pd.read_csv(os.path.join(project_dir,'data_splits.csv'))
     
-    # these ProbSomatic hyperparameters are reasonably close to the optimal parameters found using random search.
+    # these default ProbSomatic hyperparameters are reasonably close to the optimal parameters found using random search.
     # Using these hyperparams, the resulting output works quite well as a feature in the downstream machine learning model. 
-    prob_somatic_hyperparams = PROB_SOMATIC_HYPERPARAMS
+    if hyperparams is None:
+        prob_somatic_hyperparams = PROB_SOMATIC_HYPERPARAMS
+    else:
+        prob_somatic_hyperparams = hyperparams
     print(prob_somatic_hyperparams)
-    prob_somatic_annotated = multiprocess_samples(data, params=prob_somatic_hyperparams, max_cores = max_cores)
+    prob_somatic_annotated = multiprocess_samples(data_splits, params=prob_somatic_hyperparams, max_cores = max_cores)
     if output_dir is None: 
         prob_somatic_output_dir = os.path.join(project_dir, 'mafs/prob_somatic/') 
     else:
